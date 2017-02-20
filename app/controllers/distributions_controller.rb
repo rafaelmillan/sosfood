930a06738cdacc78191c9f0db6096d580bc4ca11
Xuan Bich MAI THI <xuanbichmai@gmail.com>
2017-02-20 17:47:47 +0100

    Controller and views for Distribution created

---
+++ app/controllers/distributions_controller.rb
@@ -0,0 +1,49 @@
+class DistributionsController < ApplicationController
+  before_action :set_distribution, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @distributions = Distribution.all
+  end
+
+  def show
+  end
+
+  def new
+    @distribution = Distribution.new
+  end
+
+  def create
+    @distribution = Distribution.create(distribution_params)
+    if @distribution.save
+      redirect_to distribution_path(@distribution)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @distribution = Distribution.update(distribution_params)
+    if @distribution.save
+      redirect_to distribution_path(@distribution)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @distribution.destroy
+  end
+
+  private
+
+  def set_distribution
+    @distribution = Distribution.find(params[:id])
+  end
+
+  def distribution_params
+    params.require(:distribution).permit(:name, :address_1, :address_2, :postal_code, :city, :country, :organization)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :organizations
-  root to: 'pages#home'
+  root to: 'distributions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :distributions
 end
