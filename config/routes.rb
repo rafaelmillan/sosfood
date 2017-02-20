b2472757c77527cefe87892748d274f3d6bb088a
Paule Emma DULOIR <pauleemma.duloir@gmail.com>
2017-02-20 17:30:23 +0100

    show for controller organization

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :organizations
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :organizations, only: [:show]
 end
