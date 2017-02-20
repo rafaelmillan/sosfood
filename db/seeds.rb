53a05a5a0f884d0c0ce5a3e3ffd8f7e274768408
Rafael Millan <rafmillan@gmail.com>
2017-02-20 18:38:24 +0100

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Faker::UniqueGenerator.clear
+
+5.times do
+  org = Organization.new(
+    email: Faker::Internet.unique.email,
+    password: 123456,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    organization_name: Faker::Company.name,
+    phone_number: 123456
+  )
+  org.save!
+end
+
+Organization.all.each do |org|
+  3.times do
+    dis = Distribution.new(
+      address_1: Faker::Address.street_address,
+      postal_code: Faker::Address.postcode,
+      city: "Paris",
+      country: "France",
+      station: Faker::Address.city,
+      organization: org
+    )
+    dis.save!
+  end
+end
