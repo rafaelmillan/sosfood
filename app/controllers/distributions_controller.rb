abeeb7ef6d048b1275c66802f263e44ec603d6dd
Rafael Millan <rafmillan@gmail.com>
2017-02-21 16:41:30 +0100

    Finish recurrence generator

--- app/controllers/distributions_controller.rb
+++ app/controllers/distributions_controller.rb
@@ -13,7 +13,6 @@ class DistributionsController < ApplicationController
   end
 
   def create
-    raise
     @distribution = Distribution.create(distribution_params)
     @distribution.organization = current_organization
     if @distribution.save
@@ -47,10 +46,52 @@ class DistributionsController < ApplicationController
 
   def distribution_params
     params.require(:distribution).permit(:name, :address_1, :address_2, :postal_code, :city, :country, :station, :date, :frequency, :start_time, :end_time, :weekdays, :monthdates)
+    generate_recurrence
   end
 
   def generate_recurrence
+    now = Time.now
 
+    freq = params[:distribution][:frequency]
+
+    start_hour = params[:distribution][:start_time][0..1]
+    start_min = params[:distribution][:start_time][3..4]
+
+    end_hour = params[:distribution][:end_time][0..1]
+    end_min = params[:distribution][:end_time][3..4]
+
+    day = params[:distribution][:date][0..1]
+    month = params[:distribution][:date][3..4]
+    year = params[:distribution][:date][6..7]
+
+    weekdays = params[:distribution][:weekdays]
+
+    if freq == "Once"
+      time = Time.new(year, month, day, start_hour, start_min)
+      duration = Time.new(year, month, day, end_hour, end_min) - time
+      schedule = IceCube::Schedule.new(time, duration: duration)
+    elsif freq == "Daily"
+      time = Time.new(now.year, now.month, now.day, start_hour, start_min)
+      duration = Time.new(now.year, now.month, now.day, end_hour, end_min) - time
+      schedule = IceCube::Schedule.new(time, duration: duration)
+      schedule.rrule(IceCube::Rule.daily)
+    elsif freq == "Weekly"
+      time = Time.new(now.year, now.month, now.day, start_hour, start_min)
+      duration = Time.new(now.year, now.month, now.day, end_hour, end_min) - time
+      schedule = IceCube::Schedule.new(time, duration: duration)
+      schedule.rrule(IceCube::Rule.weekly.day(:monday)) if weekdays.include?("Mon")
+      schedule.rrule(IceCube::Rule.weekly.day(:tuesday)) if weekdays.include?("Tue")
+      schedule.rrule(IceCube::Rule.weekly.day(:wednesday)) if weekdays.include?("Wed")
+      schedule.rrule(IceCube::Rule.weekly.day(:thursday)) if weekdays.include?("Thu")
+      schedule.rrule(IceCube::Rule.weekly.day(:friday)) if weekdays.include?("Fri")
+      schedule.rrule(IceCube::Rule.weekly.day(:saturday)) if weekdays.include?("Sat")
+      schedule.rrule(IceCube::Rule.weekly.day(:sunday)) if weekdays.include?("Sun")
+      raise
+    elsif freq == "monthly"
+      schedule = IceCube::Schedule.new
+    end
+
+    return schedule
   end
 
 end
