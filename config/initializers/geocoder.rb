7178154bc0aeb9cb0dff400079cc5072f853fc2f
Paule Emma DULOIR <pauleemma.duloir@gmail.com>
2017-02-21 19:04:09 +0100

    autocomplete almost done tomorrow review the show/markers

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -18,4 +18,9 @@ Geocoder.configure(
   # Calculation options
   :units => :km,
   # distances: :linear          # :spherical or :linear
+  :lookup    => :google,
+  :api_key   => ENV['GOOGLE_API_SERVER_KEY'],
+  :use_https => true,
+
 )
+
