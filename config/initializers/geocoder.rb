818af89b18b218b973ffca767337b32158526cc4
Rafael Millan <rafmillan@gmail.com>
2017-02-22 11:22:12 +0100

    update distribution model for geocoding

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -20,7 +20,6 @@ Geocoder.configure(
   # distances: :linear          # :spherical or :linear
   :lookup    => :google,
   :api_key   => ENV['GOOGLE_API_SERVER_KEY'],
-  :use_https => true,
-
+  :use_https => true
 )
 
