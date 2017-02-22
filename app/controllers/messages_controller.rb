dd343696224ae8b3d3805de80811d345471d3e76
Rafael Millan <rafmillan@gmail.com>
2017-02-22 16:41:09 +0100

    Finding soups accordign to location

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -18,10 +18,82 @@ class MessagesController < ApplicationController
     lat = coordinates[0]
     lon = coordinates[1]
 
-    Distribution.near(coordinates)
+    generate_message(coordinates)
   end
 
-  def create
+  private
+
+  def generate_message
+    distributions = Distribution.near(coordinates)
+
+    now = Time.now
+
+    breakfast_min = Time.new(now.year, now.month, now.day, 06, 00)
+    breakfast_max = Time.new(now.year, now.month, now.day, 11, 00)
+    lunch_min = Time.new(now.year, now.month, now.day, 11, 00)
+    lunch_max = Time.new(now.year, now.month, now.day, 17, 00)
+    dinner_min = Time.new(now.year, now.month, now.day, 18, 00)
+    dinner_max = Time.new(now.year, now.month, now.day, 23, 00)
+
+    if now.hour < 9
+      # break lunch dinner
+    elsif now.hour < 12
+      # lunch diner breakfast+
+      breakfast_min += 1.day
+      breakfast_max += 1.day
+    elsif now.hour < 19
+      # dinner breakfast+ lunch+
+      breakfast_min += 1.day
+      breakfast_max += 1.day
+      lunch_min += 1.day
+      lunch_max += 1.day
+    elsif now.hour >= 19
+      # break+ lunch+ dinner+
+      breakfast_min += 1.day
+      breakfast_max += 1.day
+      lunch_min += 1.day
+      lunch_max += 1.day
+      dinner_min += 1.day
+      dinner_max += 1.day
+    end
+
+    # Finding breakfast
+    distributions.each do |dis|
+      schedule = IceCube::Schedule.from_yaml(distribution.recurrence)
+      if schedule.occurs_between?(breakfast_min, breakfast_max)
+        breakfast = {
+          distribution = distribution,
+          time = schedule.next_occurrence(breakfast_min)
+        }
+      end
+      break unless breakfast == nil
+    end
+
+    # Finding lunch
+    distributions.each do |dis|
+      schedule = IceCube::Schedule.from_yaml(distribution.recurrence)
+      if schedule.occurs_between?(lunch_min, lunch_max)
+        lunch = {
+          distribution = distribution,
+          time = schedule.next_occurrence(lunch_min)
+        }
+      end
+      break unless lunch == nil
+    end
+
+    # Finding dinner
+    distributions.each do |dis|
+      schedule = IceCube::Schedule.from_yaml(distribution.recurrence)
+      if schedule.occurs_between?(dinner_min, dinner_max)
+        dinner = {
+          distribution = distribution,
+          time = schedule.next_occurrence(dinner_min)
+        }
+      end
+      break unless dinner == nil
+    end
+
+
 
   end
 end
