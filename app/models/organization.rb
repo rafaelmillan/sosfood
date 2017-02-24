bc353d11928560e0e75f31fb2ff1a4f523dc7ba3
Rafael Millán <rafmillan@gmail.com>
2017-02-24 15:00:09 +0100

    Removed has_many recurrences

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,7 +2,6 @@ class Organization < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :distributions, dependent: :destroy
-  has_many :recurrences, through: :distributions
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
