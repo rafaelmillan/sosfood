3a5794836b5dace7f40df18d7e0ce29f3092eeb2
Paule Emma DULOIR <pauleemma.duloir@gmail.com>
2017-02-24 14:57:32 +0100

    form and organization update

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,6 +2,7 @@ class Organization < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :distributions, dependent: :destroy
+  has_many :recurrences, through: :distributions
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
