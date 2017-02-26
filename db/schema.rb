5a6095f354480e0208e604aa1c93c6be438f96d1
Rafael Millan <rafmillan@gmail.com>
2017-02-26 18:40:18 +0100

    Remove station from db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170226164222) do
+ActiveRecord::Schema.define(version: 20170226173737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20170226164222) do
     t.string   "postal_code"
     t.string   "city"
     t.string   "country"
-    t.string   "station"
     t.integer  "organization_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
