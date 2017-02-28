a907af6f1d9dd2dfb6d3a0af36c42fdc56afc305
Rafael Millan <rafmillan@gmail.com>
2017-02-28 15:58:56 +0100

    Refactor sms service and add exceptions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,22 +4,30 @@ class MessagesController < ApplicationController
   skip_after_action :verify_authorized, only: :receive
 
   def receive
-    body = params["Body"]
-    sender = params["From"]
-    test_mode = params["test_mode"] == "true" ? true : false
+     # Verifies http request comes from Twilio
     if params["key"] == ENV['SMS_WEBHOOK_KEY']
-      if Recipient.find_by(phone_number: sender) # If recipient exists
-        recipient = Recipient.find_by(phone_number: sender)
-      else # If recipient is new
-        recipient = Recipient.new(phone_number: sender)
-        recipient.save
-      end
-      message = Message.new(content: body, sent_by_user: true, recipient: recipient)
-      message.save
-      coordinates = Geocoder.coordinates(body + " France")
 
-      twilio_service = TwilioService.new
-      twilio_service.send_message(message.recipient.phone_number, coordinates, test_mode)
+      sender = params["From"]
+      body = params["Body"]
+
+      message_processor = MessageProcessingService.new
+      message_details = message_processor.process(body, sender)
+
+      # Send message
+      test_mode = params["test_mode"] == "true" ? true : false
+      if test_mode
+        puts message_details[:body]
+      else
+        @client = Twilio::REST::Client.new ENV['TWILIO_ACCCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+
+        @client.account.messages.create(
+          from: '+33644647897',
+          to: message_details[:recipient].phone_number,
+          body: message_details[:body]
+        )
+      end
+
     end
   end
+
 end
