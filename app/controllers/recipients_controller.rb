65f724589368731a69451f1eee2a64a9a2ee0b78
Paule Emma DULOIR <pauleemma.duloir@gmail.com>
2017-02-28 11:34:22 +0100

    btn scroll for home done

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -11,10 +11,7 @@ class RecipientsController < ApplicationController
       recipient.save
     else #si mon user n'existe pas je crée un new user
       recipient = Recipient.new(suscribed: :true, phone_number: :phone_number, address: :address )
-      #je change son statut à suscribed
-      #j'attribue un numero de telephone au user
-      #j'attribue une lat et une long au user
-      #je le save
+
       recipient.save
     end
 
