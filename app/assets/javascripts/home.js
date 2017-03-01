9f14bcbac83b38df1e8e7e57951a09380960753d
Paule Emma DULOIR <pauleemma.duloir@gmail.com>
2017-03-01 17:23:50 +0100

    scroll btn for homepage

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,7 +2,7 @@ $(function() {
   $("#btn-scroll").click(function(e) {
     e.preventDefault;
     $('html, body').animate({
-      scrollTop: $('#search-section').offset().top}, 'slow'
+      scrollTop: $('#comment-ca-marche').offset().top}, 'slow'
       );
   });
 
