ef75da4ab11ffd654b72288c48bd534f17aba289
Rafael Millan <rafmillan@gmail.com>
2017-03-10 16:48:59 +0100

    Avoid forms to be sent on google autocomplete when enter is pressed

--- app/mailers/distribution_mailer.rb
+++ app/mailers/distribution_mailer.rb
@@ -7,9 +7,10 @@ class DistributionMailer < ApplicationMailer
   #
   def accept(user, distribution)
     @user = user
+    email = @user.nil? ? "contact@sos-food.org" : @user.email
     @distribution = distribution
 
-    mail(to: @user.email, subject: "Votre distribution a été validée")
+    mail(to: email, subject: "Votre distribution a été validée")
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -19,15 +20,16 @@ class DistributionMailer < ApplicationMailer
   #
   def decline(user, distribution)
     @user = user
+    email = @user.nil? ? "contact@sos-food.org" : @user.email
     @distribution = distribution
 
-    mail(to: @user.email, subject: "Votre distribution n'a pas été validée")
+    mail(to: email || "contact@sos-food.org", subject: "Votre distribution n'a pas été validée")
   end
 
   def create(user, distribution)
     @user = user
     @distribution = distribution
 
-    mail(to: @user.email, subject: "Votre distribution est en cours de validation")
+    mail(to: @user.email || "contact@sos-food.org", subject: "Votre distribution est en cours de validation")
   end
 end
