978c41da24040b3f588281a2bea83c6841a4c2bd
Rafael Millan <rafmillan@gmail.com>
2017-03-10 16:21:16 +0100

    Set up Postmark

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sosfood_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'www.sos-food.org' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
