8345e1701308a518be12c9dea2cf7160729cab06
Rafael Millan <rafmillan@gmail.com>
2017-03-16 11:50:16 +0100

    Add beta page

--- app/assets/javascripts/app/home.js
+++ app/assets/javascripts/app/home.js
@@ -2,7 +2,7 @@ $(function() {
   $("#btn-scroll").click(function(e) {
     e.preventDefault;
     $('html, body').animate({
-      scrollTop: $('#comment-ca-marche').offset().top - 50}, 'slow', 'easeOutExpo'
+      scrollTop: $('#comment-ca-marche').offset().top - 50}, 'slow'
       );
   });
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
-  skip_after_action :verify_authorized, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :beta]
+  skip_after_action :verify_authorized, only: [:home, :beta]
 
   def home
+  end
+
+  def beta
   end
 end
