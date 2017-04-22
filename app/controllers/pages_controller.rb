cb52c17264f2ee6fa5d356139b0cb243f2c2bac6
Rafael Millan <rafmillan@gmail.com>
2017-04-22 18:33:45 +0200

    Add maps to home cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,10 @@ class PagesController < ApplicationController
   skip_after_action :verify_authorized, only: [:home, :beta]
 
   def home
-    @distributions = Distribution.all.reject{|d| d.schedule.next_occurrence.nil? }.sort_by {|d| d.schedule.next_occurrence.start_time }[0..2]
+    @distributions = Distribution
+      .where.not(latitude: nil, longitude: nil)
+      .reject{|d| d.schedule.next_occurrence.nil? }
+      .sort_by {|d| d.schedule.next_occurrence.start_time }[0..2]
   end
 
   def beta
